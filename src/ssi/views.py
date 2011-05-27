8cc3419f8704724fe02575f75421051558da48e0
Simon de Haan <simon@praekeltfoundation.org>
2011-05-27 17:50:38 +0200

    defaulting to RequestContext instead of Context for ssi rendering

--- src/ssi/views.py
+++ src/ssi/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
-from django.template import Template, Context
+from django.template import Template, RequestContext
 from django.core.cache import cache
 
 def render_from_cache(request, cache_key):
     template = Template(cache.get(cache_key, ''))
-    context = cache.get('%s:context' % cache_key, Context({}))
-    return HttpResponse(template.render(context))
+    context = cache.get('%s:context' % cache_key, {})
+    request_context = RequestContext(request, context)
+    return HttpResponse(template.render(request_context))
     
